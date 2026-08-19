211322c98021a8206a40bcfdea359dbd453e9811
Huilustudio <markku@huilustudio.net>
2026-08-19 15:12:38 +0300

    78989

--- pikakirjoitin/sw.js
+++ pikakirjoitin/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pikakirjoitin-offline-v1.1.29-copy-paste-end';
+const CACHE_NAME = 'pikakirjoitin-offline-v1.1.30-write-paste-end';
 const APP_SHELL = [
   './',
   './index.html',
