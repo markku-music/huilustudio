0c4af26887edf4522ba879e7e37de47253a60a49
Huilustudio <markku@huilustudio.net>
2026-08-23 14:48:28 +0300

    ertert

--- pikakirjoitin/sw.js
+++ pikakirjoitin/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pikakirjoitin-offline-v1.1.74-projektin-avaus-ipad-korjattu';
+const CACHE_NAME = 'pikakirjoitin-offline-v1.1.75-monisivu-scrollaus-korjattu';
 const APP_SHELL = [
   './',
   './index.html',
