4ae368be243b307712d224399480647cf1e3226c
Huilustudio <markku@huilustudio.net>
2026-09-08 13:47:38 +0300

    ghjghj

--- lentokone/sw.js
+++ lentokone/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lentokone-pwa-base-4.0-ios-audio-fix-1';
+const CACHE_NAME = 'lentokone-pwa-base-4.0-huilu-pasuuna-calibration-master-mute-2';
 const APP_SHELL = [
   './',
   './index.html',
