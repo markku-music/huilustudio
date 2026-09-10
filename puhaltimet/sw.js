725499ba5dc098e8a35f9c320dd664682112b21d
Huilustudio <markku@huilustudio.net>
2026-09-10 09:26:06 +0300

    rtyrty

--- puhaltimet/sw.js
+++ puhaltimet/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='puhaltimet-';
-const CACHE='puhaltimet-0.1.1';
+const CACHE='puhaltimet-0.1.4-instrument-refresh';
 const ASSETS=['./','index.html','manifest.webmanifest','opensheetmusicdisplay.min.js','sample_button.webp','icons/icon-192.png','icons/icon-512.png','icons/icon-512-maskable.png','icons/apple-touch-icon.png','instruments/huilu.webp','instruments/klarinetti.webp','instruments/saksofoni.webp','instruments/oboe.webp','instruments/fagotti.webp','instruments/trumpetti.webp','instruments/alttotorvi.webp','instruments/kayratorvi.webp','instruments/pasuuna.webp','instruments/tuuba.webp'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(CACHE_PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
